8dd743ec4d29a71a1693c02531af587619448986
sx042f <simran.satpute@pdgc.com>
2020-11-26 18:13:24 +0530

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { NgbAlert, NgbDate } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-bootstrap-practice';
+  private _success = new Subject<string>();
+
+  isCollapsed = false;
+  selectedDate: String;
+
+  staticAlertClosed = false;
+  successMessage = '';
+
+  @ViewChild('staticAlert', { static: false }) staticAlert: NgbAlert;
+  @ViewChild('selfClosingAlert', { static: false }) selfClosingAlert: NgbAlert;
+
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlert.close(), 20000);
+
+    this._success.subscribe((message) => (this.successMessage = message));
+    this._success.pipe(debounceTime(5000)).subscribe(() => {
+      if (this.selfClosingAlert) {
+        this.selfClosingAlert.close();
+      }
+    });
+  }
+
+  changeSuccessMessage() {
+    this._success.next(`${new Date()} - Message successfully changed.`);
+  }
+
+  onDateSelect(date: NgbDate) {
+    this.selectedDate = this.formatDate(new Date(date.year, date.month - 1, date.day));
+    console.info('this.selectedDate: ', this.selectedDate);
+    return date;
+  }
+
+  formatDate(date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
 }
